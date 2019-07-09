723630e693cb3dad5dc992b2cb37c1280d931a17
Bader <mrbadernasser@gmail.com>
2019-07-09 20:43:07 +0300

    Restaurant listing

--- restaurants/views.py
+++ restaurants/views.py
@@ -5,15 +5,19 @@ def welcome(request):
 
 def restaurant_list(request):
 
-    context = {
+    context = { "my_list" : [
+    {"restaurant_name":"Hurdees", "food_type" : "Fast"},
+    {"restaurant_name":"Burkin", "food_type" : "Dry"},
+    {"restaurant_name":"McDont", "food_type" : "Poison"}
+    ]
 
     }
-    return render(request, 'list.html', context)
+    return  render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
 
-    context = {
+    context = { "my_object" : {"restaurant_name":"Hurdees", "food_type":"Fast"} 
 
     }
     return render(request, 'detail.html', context)
